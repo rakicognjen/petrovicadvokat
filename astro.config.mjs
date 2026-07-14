10de3b37a24be97a6827323450db68f400b3b47c
rakicognjen <ognjenrakic011@gmail.com>
2026-07-14 11:44:18 +0200

    Uklonjeno ogranicavanje na beograd ili drugi osnovni sud

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://advokatpetrovic.rs',
+  site: 'https://petrovicadvokat.rs',
   integrations: [sitemap()]
 });
